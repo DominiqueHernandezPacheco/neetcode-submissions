456c37e45583183b998423234807ee6a07852db7
Dominique Hernández <148931049+DominiqueHernandezPacheco@users.noreply.github.com>
2026-08-04 06:49:55 -0600

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-9.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer,Integer> numbers=new HashMap<>();
+
+        for(int num:nums){
+            if (numbers.containsKey(num)){
+             
+            return true;
+        }else{
+            numbers.put(num,1);
+             }
+
+        }
+        return false;
+    }
+} 
