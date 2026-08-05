0aea5bb8980232b35827972408ea73eec84d12f9
Dominique Hernández <148931049+DominiqueHernandezPacheco@users.noreply.github.com>
2026-08-05 16:18:42 -0600

    Add: two-integer-sum - submission-16

---
+++ Data Structures & Algorithms/two-integer-sum/submission-16.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        int [] numRes=new int[2];
+        int search;
+        int index=0;
+        HashMap <Integer, Integer> numeros=new HashMap<>();
+
+        for (int num:nums){
+            search=target-num;
+            if(numeros.containsKey(search)){
+                numRes[0]=numeros.get(search);
+            numeros.put(num, index);
+                numRes[1]=numeros.get(num);
+                break;
+            } else{
+                numeros.put(num,index);
+                index++;
+            }
+            
+        }
+
+        return numRes;
+    } 
+    
+}
