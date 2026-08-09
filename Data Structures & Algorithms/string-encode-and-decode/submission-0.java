0ffe3c6c48ecab52c64dfd068ff3c64357b375df
Dominique Hernández <148931049+DominiqueHernandezPacheco@users.noreply.github.com>
2026-08-08 19:01:49 -0600

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        String cadena;
+
+        for (String str : strs) {
+            sb.append(str.length());
+            sb.append('#');
+            sb.append(str);
+        }
+        cadena = sb.toString();
+        return cadena;
+    }
+
+    public List<String> decode(String str) {
+        String cadena=str;
+        int i = 0;
+        int j = 0;
+        List<String> decoded = new ArrayList<>();
+
+        while ( j < cadena.length()) {
+            if (cadena.charAt(j) != '#') {
+                j++;
+            } else {
+                if (cadena.charAt(j) == '#') {
+                    int ciclado = Integer.parseInt(cadena.substring(i,j));
+                    String palabra = cadena.substring(j + 1, j+1+ciclado);
+                    decoded.add(palabra);
+                    i = j +1 + ciclado;
+                    j = i + 1;
+                }
+            }
+        }
+        return decoded;
+    }
+}
