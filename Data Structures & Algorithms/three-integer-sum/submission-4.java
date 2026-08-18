35f971b4a36605a82d72556d4f6e987e31cba733
Dominique Hernández <148931049+DominiqueHernandezPacheco@users.noreply.github.com>
2026-08-18 09:09:17 -0600

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> triplets = new ArrayList<>();
+
+        for (int k=0; k<nums.length;k++) {
+            int i = 0;
+            int j = nums.length - 1;
+            while (i < j) {
+                if (i == k) {
+                    i++;
+                    continue;
+                }
+
+                if (j == k) {
+                    j--;
+                    continue;
+                }
+
+                if (nums[k] + nums[i] + nums[j] == 0) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[k]);
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+
+                    Collections.sort(temp);
+
+                    if (!triplets.contains(temp)) {
+                        triplets.add(temp);
+                    }
+
+                    i++;
+                    j--;
+
+                } else {
+                    if (nums[k] + nums[i] + nums[j] > 0) {
+                        j--;
+                        continue;
+                    }
+
+                    if (nums[k] + nums[i] + nums[j] < 0) {
+                        i++;
+                        continue;
+                    }
+                }
+            }
+        }
+        return triplets;
+    }
+}
